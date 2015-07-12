384cd8b2fc9fe16d736fca8557a3c2f7ee0becbd
PedroH3PedroM <deathangel623@gmail.com>
2015-07-12 11:30:19 -0300

    Gabriel Nantes/Gabriel Augusto
    proximoperfeito($n)

--- miscelanea.class.php
+++ miscelanea.class.php
@@ -5,5 +5,19 @@
     private function inverte($palavra){
 	$this ->inverte = strrev($palavra);
 	}
+	private $primo;
+	private $numero;
+	private $n;
+	private $a
+	private function($n);{
+	this ->a=0;
+	this ->a++;
+	do {
+	this ->primo=gmp_nextprime ( int $a );
+	this ->n=2^($primo-1)*(2^$primo − 1);
+	this ->a++;
+	}while (this ->n>this ->numero);
+	}
+
     }
 ?>
